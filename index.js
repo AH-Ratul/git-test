e40806e07620153aaa8302c4e7eaa3175e80878b
AH-Ratul <ahratul21@gmail.com>
2023-02-08 20:51:06 +0600

    ahr web

---
+++ index.js
@@ -0,0 +1,13 @@
+// By NoobMaster69
+
+const txt1 = document.getElementById("input_1");
+const txt2 = document.getElementById("input_2");
+const btn = document.getElementById("submit");
+const out = document.getElementById("p")
+const sum = txt1+txt2;
+
+function btn() {
+    out.innerHTML = sum.value;
+}
+
+btn.a
